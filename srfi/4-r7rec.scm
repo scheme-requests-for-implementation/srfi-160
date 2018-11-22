a41b169dc213df781b48887262a0f6fe0c88e2e5
Arthur A. Gleckler <srfi@speechcode.com>
2018-11-22 10:43:47 -0800

    Remove at John's request.
    Note that I just added John's implementation of SRFI 4 in that SRFI's
    repository under "contrib/cowan/.

--- srfi/4-r7rec.scm
+++
@@ -1,6 +0,0 @@
-;; The representation of s8 through s64 vectors
-
-(define-record-type <multivector> (make-multivector tag bv) multivector?
-  (tag tag)
-  (bv bv))
-
--- srfi/4-r7tests.scm
+++
@@ -1,5 +0,0 @@
-(import (scheme base))
-(import (srfi 4))
-(import (scheme write))
-
-(include "srfi/4-sharedtests.scm")
