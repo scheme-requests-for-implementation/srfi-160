73d74578dd959dc7c18ba0e9dbf00b2f90d3ee4c
John Cowan <cowan@ccil.org>
2019-08-24 12:47:00 -0400

    Updated Implementation section to match the implementation

--- unexpander.sh
+++ unexpander.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Remove expanded library, implementation and test files
 for at in u8 s8 u16 s16 u32 s32 u64 s64 f32 f64 c64 c128; do
-    rm srfi/160/$at.sld
-    rm srfi/160/$at-impl.scm
-    rm srfi/160/base/$at-vector2list.scm
-    rm srfi.160.$at.scm
+    rm -f srfi/160/$at.sld
+    rm -f srfi/160/$at-impl.scm
+    rm -f srfi/160/base/$at-vector2list.scm
+    rm -f srfi.160.$at.scm
 done
 
